a2510f3b5d0040a9475104c8a6ac217da630dc17
situboy <situboy@atguigu.com>
2020-09-14 19:55:23 +0800

    在idea中的第一次提交

---
+++ demo1/src/test/java/com/atguigu/DemoTest.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+public class DemoTest {
+    public static void main(String[] args) {
+        System.out.println("hello~");
+        System.out.println("hello~");
+        System.out.println("hello~");
+        System.out.println("hello~");
+        System.out.println("hello~");
+    }
+}
