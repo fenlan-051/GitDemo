469cbd63c4e3b5d4d0782a1af3ff8d1c9ff16c0a
situboy <situboy@atguigu.com>
2020-09-14 21:27:32 +0800

    测试协同开发

--- demo1/src/main/java/com/atguigu/HelloWorld.java
+++ demo1/src/main/java/com/atguigu/HelloWorld.java
@@ -8,7 +8,7 @@ public class HelloWorld {
         System.out.println("new content");
         System.out.println("world~");//测试冲突在master
         System.out.println("world~");
-        System.out.println("world~");
+        System.out.println("world~");//测试协同开发
         System.out.println("world~");
         System.out.println("world~");
     }
