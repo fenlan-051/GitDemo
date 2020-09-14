1f535ea379748c8244714da860abef8a42c3329b
situboy <situboy@atguigu.com>
2020-09-14 20:06:09 +0800

    测试冲突在hotfix分支

--- demo1/src/main/java/com/atguigu/HelloWorld.java
+++ demo1/src/main/java/com/atguigu/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("world~");
         System.out.println("world~");
-        System.out.println("world~");
+        System.out.println("world~");//测试冲突在hotfix分支
         System.out.println("world~");
         System.out.println("world~");
         System.out.println("world~");
