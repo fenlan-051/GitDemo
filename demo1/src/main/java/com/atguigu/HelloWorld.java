15ed058d2c77ce78cc291d1514fa941f6c57ab25
situboy <situboy@atguigu.com>
2020-09-14 20:00:45 +0800

    创建一个helloworld类

---
+++ demo1/src/main/java/com/atguigu/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.atguigu;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("world~");
+        System.out.println("world~");
+        System.out.println("world~");
+        System.out.println("world~");
+        System.out.println("world~");
+        System.out.println("world~");
+    }
+}
